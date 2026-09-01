ffe0e6c3c86401fbc26ae3192b31ac75e5145ea8
Anugrahaa07 <anugraha.a2020@gmail.com>
2026-09-01 22:59:12 +0530

    runed the program again

--- Anugraha/timecomplexity/qs1.py
+++ Anugraha/timecomplexity/qs1.py
@@ -2,53 +2,47 @@ import random
 import time
 import matplotlib.pyplot as plt
 
-
-#Bubble Sort function
+# 1. Bubble Sort function
 def bubble_sort(arr):
     n = len(arr)
 
     for i in range(n):
         for j in range(0, n - i - 1):
-
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-    return arr
 
-
-#Function to create random array
+# 2. Function to create an array of random numbers
 def create_array(n):
-    arr = []
-
-    for i in range(n):
-        arr.append(random.randint(1, 1000))
-
-    return arr
+    return [random.randint(1, 1000) for _ in range(n)]
 
 
-#Lists to store results
+# Lists to store input sizes and execution times
 sizes = []
 times = []
 
-
-#Test Bubble Sort for different input sizes
-for n in range(1,5):
-
+# 3. Run bubble sort for different input sizes
+for n in range(1, 20000):
     arr = create_array(n)
-    start = time.time()
+
+    start_time = time.time()
+
     bubble_sort(arr)
-    end = time.time()
-    execution_time = end - start
+
+    end_time = time.time()
+
+    execution_time = end_time - start_time
 
     sizes.append(n)
     times.append(execution_time)
 
 
-#Plot the results
+# 4. Plot the execution time
 plt.plot(sizes, times)
 
 plt.xlabel("Array Size (n)")
 plt.ylabel("Execution Time (seconds)")
-plt.title("Bubble Sort Execution Time")
+plt.title("Execution Time of Bubble Sort")
+plt.grid(True)
 
 plt.show()
