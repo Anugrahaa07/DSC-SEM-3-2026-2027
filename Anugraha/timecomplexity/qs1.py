86bcfb6b928e39aa0db4b5a6ca26c29d9335b4d9
Anugrahaa07 <anugraha.a2020@gmail.com>
2026-09-09 04:50:14 +0530

    time complexity

--- Anugraha/timecomplexity/qs1.py
+++ Anugraha/timecomplexity/qs1.py
@@ -2,7 +2,6 @@ import random
 import time
 import matplotlib.pyplot as plt
 
-# 1. Bubble Sort function
 def bubble_sort(arr):
     n = len(arr)
 
@@ -11,18 +10,13 @@ def bubble_sort(arr):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-
-# 2. Function to create an array of random numbers
 def create_array(n):
     return [random.randint(1, 1000) for _ in range(n)]
 
-
-# Lists to store input sizes and execution times
 sizes = []
 times = []
 
-# 3. Run bubble sort for different input sizes
-for n in range(1, 20000):
+for n in range(1, 5):
     arr = create_array(n)
 
     start_time = time.time()
@@ -36,8 +30,6 @@ for n in range(1, 20000):
     sizes.append(n)
     times.append(execution_time)
 
-
-# 4. Plot the execution time
 plt.plot(sizes, times)
 
 plt.xlabel("Array Size (n)")
